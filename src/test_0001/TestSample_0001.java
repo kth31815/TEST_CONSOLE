a895bd93d90bf8b828f9548bf6844b2e8471803c
gsrvoc <gsrvoc@email.com>
2021-11-25 16:49:06 +0900

    test2233

--- src/test_0001/TestSample_0001.java
+++ src/test_0001/TestSample_0001.java
@@ -5,7 +5,13 @@ public class TestSample_0001 {
     public static void main(String[] args) {
         System.out.println("test");
 
-        System.out.println("12341235123123");
+        int re = 0;
+        int re2 = 0;
+        int re3 = 0;
+
+
+
+        System.out.println(12451);
 
     }
 
